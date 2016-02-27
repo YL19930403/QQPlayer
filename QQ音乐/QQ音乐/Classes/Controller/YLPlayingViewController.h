0192ac60661ce14d7c1959d72140bc605117f069
zhangli <313890735@qq.com>
2016-02-27 23:39:07 +0800

    搭建界面

---
+++ QQ音乐/QQ音乐/Classes/Controller/YLPlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  YLPlayingViewController.h
+//  QQ音乐
+//
+//  Created by 余亮 on 16/2/27.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLPlayingViewController : UIViewController
+
+@end
---
+++ QQ音乐/QQ音乐/Classes/Controller/YLPlayingViewController.m
@@ -0,0 +1,37 @@
+//
+//  YLPlayingViewController.m
+//  QQ音乐
+//
+//  Created by 余亮 on 16/2/27.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+#import "YLPlayingViewController.h"
+
+@interface YLPlayingViewController ()
+
+@end
+
+@implementation YLPlayingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
