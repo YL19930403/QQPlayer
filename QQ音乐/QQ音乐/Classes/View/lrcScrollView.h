7a968ce6a1d427cb225b542b97155c77a65b73da
zhangli <313890735@qq.com>
2016-02-28 16:27:10 +0800

    歌词UITableView的处理

--- QQ音乐/QQ音乐/Classes/View/lrcScrollView.h
+++ QQ音乐/QQ音乐/Classes/View/lrcScrollView.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface lrcScrollView : UIScrollView
-
+- (instancetype)initWithCoder:(NSCoder *)aDecoder ;
 @end
--- QQ音乐/QQ音乐/Classes/View/lrcScrollView.m
+++ QQ音乐/QQ音乐/Classes/View/lrcScrollView.m
@@ -9,7 +9,8 @@
 #import "lrcScrollView.h"
 #import <Masonry.h>
 
-@interface lrcScrollView ()
+@interface lrcScrollView ()<UITableViewDataSource,UITableViewDelegate>
+
 @property(nonatomic,weak) UITableView * tableV ;
 
 @end
@@ -19,7 +20,7 @@
 //从StoryBoard加载的
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
-    if (self == [super initWithCoder:aDecoder]) {
+    if (self = [super initWithCoder:aDecoder]) {
         [self  setUpTableView];
     }
     return self ;
@@ -30,21 +31,73 @@
     //1.创建tableView
     UITableView * tableV = [[UITableView alloc] init];
     self.tableV = tableV ;
-    tableV.backgroundColor = [UIColor redColor];
-    [self  setUpTableFrame];
+    self.tableV.dataSource = self ;
+    self.tableV.rowHeight = 30 ;
+    //设置tableView的上下内边距
+    self.tableV.contentInset = UIEdgeInsetsMake(self.bounds.size.height*0.5, 0, self.bounds.size.height*0.5, 0);
+    
     //2. 添加到lrcScrollView中
     [self addSubview:tableV];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    //注意点：在UIScrollView中添加UITableView，需要添加6个约束才行
+    [_tableV mas_makeConstraints:^(MASConstraintMaker *make) {
+        //1.确定TableView的位置
+        make.top.equalTo(self.mas_top) ;
+        make.left.equalTo(self.mas_left).offset(self.bounds.size.width);
+        make.height.equalTo(self.mas_height);
+        make.width.equalTo(self.mas_width);
+        
+        //2. 确定UIScrollView的滚动范围
+        make.bottom.equalTo(self.mas_bottom);
+        make.right.equalTo(self.mas_right);
+    }];
+    
+    //对UITableView背景和边线的处理，最好是放在该方法中
+    _tableV.backgroundColor = [UIColor clearColor];
+    _tableV.separatorStyle = UITableViewCellSeparatorStyleNone ;
     
 }
 
-- (void) setUpTableFrame
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    [_tableV mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.top.equalTo(self) ;
-//        make.left.equalTo(self.mas_centerX);
-//        make.bottom.equalTo(self.mas_bottom);
-//        make.right.equalTo(self.mas_right);
-    }];
+    return 20 ;
 }
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * ID = @"UITableViewCell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+    }
+    cell.textLabel.text = @"的复活节是南方";
+    [cell.textLabel setTextColor:[UIColor whiteColor]];
+    [cell.textLabel setFont:[UIFont systemFontOfSize:13]];
+    cell.backgroundColor = [UIColor clearColor];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    [cell.textLabel setTextAlignment:NSTextAlignmentCenter];
+    return cell ;
+    
+}
+
+
+
+
 @end
+
+
+
+
+
+
+
+
+
+
+
